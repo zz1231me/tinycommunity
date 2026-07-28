5c51096f3a62f772d342dba38cd7acc9b14ada95
zz1231me <zz1231me@gmail.com>
2026-07-28 15:10:24 +0900

    .

--- client/src/pages/components/calendar/CalendarPage.tsx
+++ client/src/pages/components/calendar/CalendarPage.tsx
@@ -318,7 +318,7 @@ const CalendarPage: React.FC = () => {
               weekends={true}
               fixedWeekCount={false}
               showNonCurrentDates={false}
-              dayMaxEvents={3}
+              dayMaxEvents={4}
               noEventsContent={renderNoEvents}
               dayCellClassNames={dayCellClassNames}
             />
